5b1c56e680d3a8612712d121b2780c53c9a14a90
TheSerch07 <jejog50@gmail.com>
2022-06-04 20:20:26 -0500

    frontend

--- client/src/components/Countries.jsx
+++ client/src/components/Countries.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { fetchCountries} from "../store/actions"
 import Country from "./Country";
 
 export default function Countries() {
+    const [currentPage, setCurrentPage] = useState(1)
+    
     let countries = useSelector((state) => state.filteredCountries)
     let dispatch = useDispatch()
     useEffect(() => {
